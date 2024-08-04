4c3e587930242c34618418cd77236b9cf49046ba
oussamachaab <oussamachaab378@gmail.com>
2024-08-04 10:09:19 +0100

    final_

--- excel_xml.py
+++ excel_xml.py
@@ -1,44 +1,5 @@
 import pandas as pd
-from lxml import etree as et
-import os
-import openpyxl
 import xml.etree.ElementTree as ET
-'''
-raw_data = pd.read_excel(r'Grades.xlsx')  #read excel
-print(raw_data)
-
-root = et.Element('odoo')                   #create odoo balise
-root_tags0 = et.SubElement(root, 'data',  no_update="1")    #create data balise
-print(root) 
-
-for row in raw_data.iterrows():                         #loop for each tag
-    root_tags = et.SubElement(root_tags0, 'record', id="artec_produit" , model="product product")     #=== > employe Root name
-    print(root_tags)
-
-# These are the tag names for each row (SECTION 1)
-    Column_heading_1 = et.SubElement(root_tags, 'field', name="Name")
-    Column_heading_2 = et.SubElement(root_tags, 'field', name="active", eval="acive")
-    Column_heading_3 = et.SubElement(root_tags, 'filed', name="price")
-
-###These are the values that will be populated for each row above
-# The values inside the [] are the raw file column headings.(SECTION 2)
-    Column_heading_1.text = str(row[1]['Name'])
-    Column_heading_2.text = str(row[1]['active'])
-    Column_heading_3.text = str(row[1]['price'])
-
-for record in root.iter('id'):
-
-    new_rank = int(record.text) + 1
-
-    record.text = str(new_rank)
-
-    record.set('updated', 'yes')
-
-tree = et.ElementTree(root) #==> The variable tree is to hold all the values of "root"
-et.indent(tree, space="\t", level=0) #===> This just formats in a way that the XML is readable
-tree.write('output.xml', encoding="utf-8") #==> The data is saved to an XML file
-'''
-
 
 
 r_data = pd.read_excel(r'Grades.xlsx')  #read excel
@@ -81,35 +42,3 @@ for id, row in r_data.iterrows():    #looping the rows
 tree = ET.ElementTree(odoo)
 tree.write('xml_final.xml') 
 
-
-
-'''
-
-raw_data = pd.read_excel(r'Grades.xlsx')  #read excel
-print(raw_data)
-
-root = et.Element('odoo')                   #create odoo balise
-root_tags0 = et.SubElement(root, 'data',  no_update="1")    #create data balise
-print(root) 
-
-for row in raw_data.iterrows():                         #loop for each tag
-    root_tags = et.SubElement(root_tags0, 'record', id="artec_produit" , model="product product")     #=== > employe Root name
-    print(root_tags)
-
-# These are the tag names for each row (SECTION 1)
-    Column_heading_1 = et.SubElement(root_tags, 'field', name="Name")
-    Column_heading_2 = et.SubElement(root_tags, 'field', name="active", eval="acive")
-    Column_heading_3 = et.SubElement(root_tags, 'filed', name="price")
-
-###These are the values that will be populated for each row above
-# The values inside the [] are the raw file column headings.(SECTION 2)
-    Column_heading_1.text = str(row[1]['Name'])
-    Column_heading_2 = row[1]['active']
-    Column_heading_3.text = str(row[1]['price'])
-
-
-tree = et.ElementTree(root) #==> The variable tree is to hold all the values of "root"
-et.indent(tree, space="\t", level=0) #===> This just formats in a way that the XML is readable
-tree.write('output.xml', encoding="utf-8") #==> The data is saved to an XML file
-
-'''
